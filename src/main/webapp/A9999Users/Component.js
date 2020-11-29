cf3d66e12690e4a32e88706060a16664af43c584
SeriousSoftware <vdenberg+github@gmail.com>
2020-11-29 22:33:09 +0100

    Feat: 12 Some improvements, running conditioning.

--- src/main/webapp/A9999Users/Component.js
+++ src/main/webapp/A9999Users/Component.js
@@ -20,6 +20,12 @@ sap.ui.define([
             // call the base component's init function
             UIComponent.prototype.init.apply(this, arguments);
 
+            const cldr = sap.ui.core.LocaleData.getInstance(sap.ui.getCore().getConfiguration().getFormatSettings().getFormatLocale());
+            //  Date picker date to start with Monday
+            cldr.mData["weekData-firstDay"] = 1;
+            // DateRangeSelection insensitive for this change
+            cldr.mData["weekData-minDays"] = 4;
+
             // set the device model
             this.setModel(models.createDeviceModel(), "device");
 
